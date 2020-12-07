945e02330494896b0b6489329f7adb0bf05404e3
Doshchinsky <akter1997@gmail.com>
2020-12-08 06:48:00 +0700

    Added some changes from new ver

--- src/include/gui.py
+++ src/include/gui.py
@@ -11,8 +11,8 @@ class Window(Frame):
     def initUI(self):
         self.master.title("WiFi Positioning")
         self.pack(fill=BOTH, expand=1)
-        self.map = ImageTk.PhotoImage(Image.open("data/images/map.png"))
-        self.layer = Canvas(self, width=934, height=312)
+        self.map = ImageTk.PhotoImage(Image.open("data/images/map_modded.png"))
+        self.layer = Canvas(self, width=1200, height=1023)
         self.layer.create_image(0, 0, anchor=NW, image=self.map)
         self.layer.pack(fill=BOTH, expand=1)
 
@@ -23,10 +23,10 @@ class Window(Frame):
                 posX = position[0]
                 posY = position[1]
 
-# pixelX = Top left angle of the blue box on map + N Square * Grid Multiply - Half of the phone's img size
-# pixelY = Top left angle of the blue box on map + N Square * Grid Multiply - Half of the phone's img size
-                pixelX = 394 + posX * 28 - 8
-                pixelY = 45 + posY * 27 - 13
+                # pixelX = Top left angle of the blue box on map + N Square * Grid Multiply - Half of the phone's img size
+                # pixelY = Top left angle of the blue box on map + N Square * Grid Multiply - Half of the phone's img size
+                pixelX = 291 + posX * 30.1 - 8
+                pixelY = 22 + posY * 30.1 - 13
 
                 self.layer.create_image(pixelX, pixelY, anchor=NW, image=self.agent)
                 self.layer.create_text(pixelX+4, pixelY+7, anchor=NW, text=position_list.index(position)+1)
